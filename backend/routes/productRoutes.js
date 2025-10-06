3aaa3456b1d01ee20e0dbfd13f5fa90bdac4c6bc
Ahmad <aa2997168@gmail.com>
2025-10-06 15:21:33 +0300

    product apis setup

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,9 +1,12 @@
 import express from "express";
-import { getAllProducts, createProduct } from "../controllers/productController.js";
+import { getProducts, createProduct, getProduct, updateProduct, deleteProduct } from "../controllers/productController.js";
 
 const router = express.Router();
 
-router.get("/", getAllProducts);
+router.get("/", getProducts);
+router.get("/:id", getProduct); // Assuming you want to get a product by ID, you might want to implement this in the controller
 router.post("/", createProduct);
+router.put("/:id", updateProduct);
+router.delete("/:id", deleteProduct);
 
 export default router;
